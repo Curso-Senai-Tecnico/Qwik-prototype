fa3e04c06cb3e6bbdea5d6c4498623a564c71165
NevermoreRaven12 <tiojoaobrb@gmail.com>
2025-10-01 10:58:22 -0300

    [FE] Finalizado design estático da Landing Page #11

--- Quik/frontend/react-app/src/pages/LandingPage.jsx
+++ Quik/frontend/react-app/src/pages/LandingPage.jsx
@@ -3,6 +3,38 @@ import { motion } from "motion/react";
 import { Link } from "react-router-dom";
 
 export default function LandingPage() {
+  const vagas = [
+    {
+      id: 1,
+      icone: "/apple.svg",
+      empresa: "Apple",
+      numeroVagas: 34,
+    },
+    {
+      id: 2,
+      icone: "/facebook.svg",
+      empresa: "Facebook",
+      numeroVagas: 176,
+    },
+    {
+      id: 3,
+      icone: "/google.svg",
+      empresa: "Google",
+      numeroVagas: 45,
+    },
+    {
+      id: 4,
+      icone: "/instagram.svg",
+      empresa: "Instagram",
+      numeroVagas: 98,
+    },
+    {
+      id: 5,
+      icone: "/netflix.svg",
+      empresa: "Netflix",
+      numeroVagas: 234,
+    },
+  ];
   return (
     <motion.div
       className="bg-gradient-to-br from-[#ffd064] via-[#ffab4b] to-[#934500] flex w-vh h-dvh bg-[length:200%_200%] justify-center items-center"
@@ -13,19 +45,24 @@ export default function LandingPage() {
         repeat: Infinity,
       }}
     >
-      <div className="bg-white w-300 h-150 ">
+      <div className="bg-white w-300 h-150 rounded-lg">
         <header>
-          <nav className="flex justify-end gap-8 pt-6">
-            <button className="italic cursor-pointer font-space">Entrar</button>
-            <button className="mr-10 bg-blue-700 text-white p-3 rounded-full italic cursor-pointer">
+          <nav className="flex justify-end gap-12 pt-6">
+            <Link to="/login" className="italic cursor-pointer font-space mt-3">
+              Entrar
+            </Link>
+            <Link
+              to="/separator"
+              className="mr-10 bg-blue-700 text-white p-3 rounded-full italic cursor-pointer"
+            >
               Cadastrar
-            </button>
+            </Link>
           </nav>
         </header>
         <main className="flex-1 grid grid-cols-2">
           <div className="flex flex-col items-center justify-center text-center ml-10">
             <img src="/esquilo.png" alt="Logo Quik" className="w-80" />
-            <h1 className="font-rammeto font-normal text-orange-300 text-5xl">
+            <h1 className="font-rammeto font-normal text-orange-300 text-5xl mb-3">
               Quik
             </h1>
             <p className="font-bold mt-3">
@@ -33,6 +70,18 @@ export default function LandingPage() {
               rápido e sem complicação. O lugar certo para sua próxima
               oportunidade profissional.
             </p>
+          </div>
+          <div className="flex flex-col justify-center items-center gap-3 mt-6">
+            {vagas.map((vaga) => (
+              <div
+                className="flex shadow-lg shadow-gray-400/60 gap-8 w-80 justify-between items-center p-6 bg-blue-700 text-white border border-white/40 backdrop-blur rounded-lg"
+                key={vaga.id}
+              >
+                <img src={vaga.icone} className="w-6 h-6 bg" />
+                <span>{vaga.empresa}</span>
+                <span>{vaga.numeroVagas}+ vagas</span>
+              </div>
+            ))}
           </div>
         </main>
       </div>
