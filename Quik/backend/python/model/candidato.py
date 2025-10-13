141c3b41985bbf77ca22fec657d5af1743a96a32
Konokari89 <95733817+Konokari89@users.noreply.github.com>
2025-10-13 19:24:13 -0300

    Delete Quik/backend/python directory

--- Quik/backend/python/model/candidato.py
+++
@@ -1,9 +0,0 @@
-from user import User
-
-class Candidato(User):
-    def __init__(self, id:int, nome:str, email:str,
-                 cpf:str, data_nascimento: str, **kwargs):
-        super().__init__(id, nome, email, **kwargs)
-        self.cpf = cpf
-        self.data_nascimento = data_nascimento
-        
--- Quik/backend/python/model/recrutador.py
+++
@@ -1,7 +0,0 @@
-from user import User
-
-class Recrutador(User):
-    def __init__(self, id:int, nome:str, email:str,
-                 cnpj:str, **kwargs):
-        super().__init__(id, nome, email, **kwargs)
-        self.cnpj = cnpj
