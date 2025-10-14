6168422d0086dc9da12d20ff2079675f54b1b52e
Pedro Paulo Cardoso Cordeiro <gon0165810@aluno.firjansenaisesi.com.br>
2025-10-13 21:25:52 -0300

    [FE] UI em beta #23

--- Quik/frontend/react-app/src/pages/candidato/CadastroCandidato.jsx
+++ Quik/frontend/react-app/src/pages/candidato/CadastroCandidato.jsx
@@ -14,11 +14,11 @@ export default function Cadastro() {
       repeat: Infinity,
     }}>
       <div
-      className='bg-white w-3xl h-11/12 rounded-md flex flex-col'
+      className='bg-white w-2xl h-11/12 rounded-md flex flex-col'
       >
         <header>
           <nav className='flex pt-10'>
-          <ChevronLeft size={48} className='cursor-pointer ml-9 mr-26' onClick={() => navigate("/")} />
+          <ChevronLeft size={48} className='cursor-pointer ml-9 mr-26 hover:scale-110 active:scale-95 transition duration-200 ease-in-out' onClick={() => navigate("/")} />
           <img src="/esquilo.png" width={160} />
             <h1 className="font-rammeto text-orange-400 text-5xl mt-20">
               Quik
@@ -30,7 +30,8 @@ export default function Cadastro() {
         <h1 className='font-inter font-bold text-2xl'>
           Prepare-se para encontrar sua próxima oportunidade!
         </h1>
-        <form className='flex flex-col gap-3'>
+        <br></br>
+        <form className='flex flex-col gap-6 items-center justify-center'>
           <input 
           type='text'
           id='nome'
@@ -53,6 +54,7 @@ export default function Cadastro() {
           name='cpf'
           placeholder='CPF'
           className='font-inter border rounded-full p-3 shadow-lg'
+          size={40}
           />
           <input
           type='password'
@@ -60,7 +62,15 @@ export default function Cadastro() {
           name='pass'
           placeholder='Senha'
           className='font-inter border rounded-full p-3 shadow-lg'
+          size={40}
           />
+          <br/>
+          <button
+          type='submit'
+          className='bg-orange-400 text-white hover:scale-110 active:scale-95 w-10/12 h-16 rounded-full font-black text-2xl text-shadow-black text-shadow-xs shadow-2xl cursor-pointer transition duration-200 ease-in-out'
+          >
+            Cadastrar
+          </button>
         </form>
         </div>
       </div>
--- Quik/frontend/react-app/src/pages/recrutador/CadastroRecrutador.jsx
+++ Quik/frontend/react-app/src/pages/recrutador/CadastroRecrutador.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import Input from '../../Input.jsx'
 
 export default function Cadastro() {
   return (
-    <div>Cadastro</div>
+    <div className='bg-red-400'>
+      <form>
+      <Input
+      type='text'
+      id='nome'
+      name='nome'
+      placeholder='Nome'
+      />
+      </form>
+    </div>
   )
 }
