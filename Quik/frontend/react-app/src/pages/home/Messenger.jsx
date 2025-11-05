4ea878cf7ff84a98a9a5fb3a1599070a47e24deb
Pedro Cardoso <tiojoaobrb@gmail.com>
2025-11-05 16:55:34 -0300

    [FE] Criado o chat de mensagem

--- Quik/frontend/react-app/src/pages/home/Messenger.jsx
+++ Quik/frontend/react-app/src/pages/home/Messenger.jsx
@@ -11,7 +11,7 @@ export default function Messenger({darkMode, setDarkMode}){
 
     return(
        <div className={`flex flex-col transition-all duration-300 fixed bottom-0 left-0`}>
-       <div className={`flex font-inter font-bold justify-between items-center ml-4 w-70 rounded-t-2xl rounded-tr-2xl cursor-pointer h-10 ${darkMode ? "bg-[#22303c] text-white": "bg-white text-black"} ${isOpen && "border-b"}`} onClick={toggleChat}>
+       <div className={`flex font-inter font-bold justify-between items-center ml-4 w-80 rounded-t-2xl rounded-tr-2xl cursor-pointer h-10 ${darkMode ? "bg-[#22303c] text-white": "bg-white text-black"} ${isOpen && "w-90 border-b"}`} onClick={toggleChat}>
         <span className='ml-4'>Chat</span>
         <div className='flex justify-center gap-1 items-center mr-4'>
             <span className=''>3</span>
@@ -24,7 +24,7 @@ export default function Messenger({darkMode, setDarkMode}){
         </div>
        </div> 
        {isOpen && 
-       <div className={`flex font-inter font-bold justify-between items-center ml-4 w-70 cursor-pointer h-90 ${darkMode ? "bg-[#22303c] text-white": "bg-white text-black"}`} onClick={toggleChat}>
+       <div className={`flex font-inter font-bold justify-between items-center ml-4 w-90 h-90 ${darkMode ? "bg-[#22303c] text-white": "bg-white text-black"}`} >
 
         </div>
         }
