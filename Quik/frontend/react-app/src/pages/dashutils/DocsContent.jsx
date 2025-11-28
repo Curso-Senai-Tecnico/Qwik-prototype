21878dabed16b718ec0b4f559d3864462bbe7467
Pedro Cardoso <tiojoaobrb@gmail.com>
2025-11-28 14:35:04 -0300

    [FE] tela de informacoes do candidato

--- Quik/frontend/react-app/src/pages/dashutils/DocsContent.jsx
+++ Quik/frontend/react-app/src/pages/dashutils/DocsContent.jsx
@@ -9,9 +9,9 @@ export default function DocsContent() {
         <div className="w-full h-full flex flex-col">
           <iframe src="/curriculo_alexandre.pdf" className="h-full w-full"/>
           <div className="flex gap-2">
-            <button className="bg-orange-500 backdrop-blur-2xl p-2 m-4 text-white shadow-2xl rounded-full hover:scale-110 active:scale-90 cursor-pointer transition-transform duration-300 ease-in-out">Enviar Currículo</button>
-            <button className="bg-orange-500 backdrop-blur-2xl p-2 m-4 text-white shadow-2xl rounded-full hover:scale-110 active:scale-90 cursor-pointer transition-transform duration-300 ease-in-out">Alterar Currículo</button>
-            <button className="bg-orange-500 backdrop-blur-2xl p-2 m-4 text-white shadow-2xl rounded-full hover:scale-110 active:scale-90 cursor-pointer transition-transform duration-300 ease-in-out">Remover Currículo</button>
+            <button className="bg-orange-500 backdrop-blur-2xl p-2 m-4 text-white shadow shadow-black rounded-full hover:scale-110 active:scale-90 cursor-pointer transition-transform duration-300 ease-in-out">Enviar Currículo</button>
+            <button className="bg-orange-500 backdrop-blur-2xl p-2 m-4 text-white shadow shadow-black rounded-full hover:scale-110 active:scale-90 cursor-pointer transition-transform duration-300 ease-in-out">Alterar Currículo</button>
+            <button className="bg-orange-500 backdrop-blur-2xl p-2 m-4 text-white shadow shadow-black rounded-full hover:scale-110 active:scale-90 cursor-pointer transition-transform duration-300 ease-in-out">Remover Currículo</button>
           </div>
         </div>
       )}
--- Quik/frontend/react-app/src/pages/dashutils/InfoContent.jsx
+++ Quik/frontend/react-app/src/pages/dashutils/InfoContent.jsx
@@ -7,11 +7,15 @@ import { Phone } from "lucide-react";
 import { Mail } from "lucide-react";
 import { MapPin } from "lucide-react";
 import { useUser } from "../../contexts/UserContext";
+import { useToken } from "../../contexts/TokenContext";
+import { CircleCheck } from "lucide-react";
+import { CircleX } from "lucide-react";
 
 export default function InfoContent() {
   const { role } = useRole();
+  const {token} = useToken();
   const [isEditing, setEditing] = useState(false);
-  const {user} = useUser()
+  const {user, setUser} = useUser()
   const [formData, setFormData] = useState({
     nascimento: user?.data_nascimento || "",
     genero: user?.genero || "",
@@ -21,13 +25,64 @@ export default function InfoContent() {
     cidade: user?.cidade || "",
     estado: user?.estado || ""
   })
+
+  const [nome, setNome] = useState({nome: user?.nome || ""})
+  const [isEditingName, setEditingName] = useState(false)
+
+  async function salvarAlteracoes() {
+    const data = {... nome, ... formData}
+    const response = await fetch ("/api/me", {
+      method: "PUT",
+      headers: {Authorization: `Bearer ${token}`, "Content-Type": "application/json"},
+      body: JSON.stringify(data)
+    })
+
+    const newData = await response.json()
+    setUser(newData)
+    setEditing(false)
+    setEditingName(false)
+  }
   
 
   
 
   return (
     <div className="w-full h-full flex flex-col">
-      {role === "recrutador" && <span>Info Recrutador</span>}
+      {role === "recrutador" && (
+        <>
+        <header className=" flex bg-gradient-to-r from-orange-400 to-orange-500 w-full h-1/10">
+          <img src={user != null ? `${user.foto}` : "/qwikpadrao.png"} className="absolute w-40 h-40 rounded-full left-2 top-4 border-4 border-white active:scale-90 cursor-pointer transition-transform duration-200 ease-in-out"/>
+        </header>
+        <main className="flex flex-col mt-25 ml-6 mr-5">
+          <div className="flex justify-between items-center">
+          <form id="formNome" className="flex">
+          <input type="text" size={10} name="nome" id="nome" className="font-inter font-bold text-2xl" disabled={!isEditingName} value={nome.nome != "" ? nome.nome : "Nome de usuário"} onChange={(e) => {setNome({nome: e.target.value})}}/>
+          {!isEditingName ?  <PencilLine onClick={() => setEditingName(true)}/> : (<>
+            <CircleCheck onClick={() => salvarAlteracoes()}/>
+            <CircleX onClick={() => setEditingName(false)} />
+            </>
+            )}
+          </form>
+          {isEditing === false && <button className="bg-orange-500 gap-2 flex p-3 rounded-full shadow shadow-black hover:scale-110 active:scale-90 transition-transform duration-200 ease-in-out cursor-pointer" onClick={() => setEditing(true)}>
+          <PencilLine color="white" size={15}/>
+          <span className="font-inter text-white text-sm">Editar Perfil</span>
+          </button>}
+          </div>
+          <span className="font-inter text-orange-400">{user != null ? user.perfil_recrutador : "Cargo na empresa"}</span>
+          <br/>
+          <h2 className="font-inter font-bold">Informações pessoais</h2>
+          <form className="grid grid-cols-2 grid-rows-6 gap-y-8 justify-between mt-5">
+
+          </form>
+          {isEditing && (
+            <div className="flex justify-end items-center gap-5">
+                <button type="submit" className="bg-orange-500 text-white font-inter p-2 rounded-md shadow shadow-black hover:scale-110 active:scale-90 transition-transform duration-200 ease-in-out cursor-pointer" onClick={() => salvarAlteracoes()}> Salvar </button>
+                <button className="bg-orange-500 text-white font-inter p-2 rounded-md shadow shadow-black hover:scale-110 active:scale-90 transition-transform duration-200 ease-in-out cursor-pointer" onClick={() => setEditing(false)}> Cancelar </button>
+            </div>
+          )}
+          </main>
+        </>
+      )}
       {role === "candidato" && (
       <>
         <header className=" flex bg-gradient-to-r from-orange-400 to-orange-500 w-full h-1/10">
@@ -35,7 +90,16 @@ export default function InfoContent() {
         </header>
         <main className="flex flex-col mt-25 ml-6 mr-5">
           <div className="flex justify-between items-center">
-          {user != null ? <span className="font-inter font-bold text-2xl "> {user.nome}</span> : <span className="font-inter font-bold text-2xl ">Nome de Usuário</span>}
+          <form id="formNome" className="flex">
+          <input type="text" size={10} name="nome" id="nome" className="font-inter font-bold text-2xl" disabled={!isEditingName} value={nome.nome != "" ? nome.nome : "Nome de usuário"} onChange={(e) => {setNome({nome: e.target.value})}}/>
+          {!isEditingName ?  <PencilLine className="hover:scale-110 transition-transform duration-200 active:scale-90 cursor-pointer" onClick={() => setEditingName(true)}/> : (<>
+            <CircleCheck className="hover:scale-110 transition-transform duration-200 active:scale-90 cursor-pointer" type="submit" onClick={(e) => {
+              e.preventDefault()
+              salvarAlteracoes()}}/>
+            <CircleX className="hover:scale-110 transition-transform duration-200 active:scale-90 cursor-pointer" onClick={() => setEditingName(false)} />
+            </>
+            )}
+          </form>
           {isEditing === false && <button className="bg-orange-500 gap-2 flex p-3 rounded-full shadow shadow-black hover:scale-110 active:scale-90 transition-transform duration-200 ease-in-out cursor-pointer" onClick={() => setEditing(true)}>
           <PencilLine color="white" size={15}/>
           <span className="font-inter text-white text-sm">Editar Perfil</span>
@@ -53,7 +117,10 @@ export default function InfoContent() {
           </div>
           )}
           <h2 className="mt-10 font-inter font-bold">Informações Pessoais</h2>
-          <form className="grid grid-cols-2 grid-rows-6 gap-y-8 justify-between mt-5">
+          <form id="info" className="grid grid-cols-2 grid-rows-6 gap-y-8 justify-between mt-5" onSubmit={(e) => {
+            e.preventDefault()
+            salvarAlteracoes()
+          }}>
             <label className="flex gap-2">
               <CalendarDays />
               <input type="text" id="nascimento" name="nascimento" disabled={!isEditing} placeholder={user != null ? user.data_nascimento : "Data de Nascimento"} value={formData.nascimento} onChange={(e) => setFormData({...formData, nascimento: e.target.value})} />
@@ -86,7 +153,7 @@ export default function InfoContent() {
 
           {isEditing && (
             <div className="flex justify-end items-center gap-5">
-                <button type="submit" className="bg-orange-500 text-white font-inter p-2 rounded-md shadow shadow-black hover:scale-110 active:scale-90 transition-transform duration-200 ease-in-out cursor-pointer" onClick={() => setEditing(false)}> Salvar </button>
+                <button type="submit" form="info" className="bg-orange-500 text-white font-inter p-2 rounded-md shadow shadow-black hover:scale-110 active:scale-90 transition-transform duration-200 ease-in-out cursor-pointer"> Salvar </button>
                 <button className="bg-orange-500 text-white font-inter p-2 rounded-md shadow shadow-black hover:scale-110 active:scale-90 transition-transform duration-200 ease-in-out cursor-pointer" onClick={() => setEditing(false)}> Cancelar </button>
             </div>
           )}
